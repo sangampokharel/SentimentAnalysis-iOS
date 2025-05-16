3b6c728cc122342db8a47216267a9984fc66f5fa
sangam <sangampokharel803@gmail.com>
2025-05-16 10:03:18 +0545

    Initial Commit

---
+++ SentimentAnalysis/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SentimentAnalysis
+//
+//  Created by EKbana on 16/05/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SentimentAnalysis/SentimentAnalysisApp.swift
@@ -0,0 +1,17 @@
+//
+//  SentimentAnalysisApp.swift
+//  SentimentAnalysis
+//
+//  Created by EKbana on 16/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct SentimentAnalysisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
