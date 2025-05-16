9844a6d1fdae81e72b41067a6776e91de2620dce
sangam <sangampokharel803@gmail.com>
2025-05-16 11:04:04 +0545

    implementation of sentiment analysis of financial news system

--- SentimentAnalysis/ContentView.swift
+++ SentimentAnalysis/ContentView.swift
@@ -6,16 +6,60 @@
 //
 
 import SwiftUI
+import CoreML
+
+
+enum labelTypes:String {
+    case positive,negative,neutral
+}
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    @State private var text = ""
+    @State private var result = ""
+    
+    let model = try! SentimentAnalysisNewsModel(configuration: MLModelConfiguration())
+    
+    private var color:Color {
+        if let label = labelTypes(rawValue: result) {
+            switch label {
+            case .positive:
+                return .green
+            case .negative:
+                return .red
+            case .neutral:
+                return .blue
+            }
         }
-        .padding()
+        return Color(UIColor.systemBackground)
+    }
+    
+    var body: some View {
+        
+        NavigationStack {
+            VStack {
+                TextField("Type here...", text: $text)
+                    .textFieldStyle(.roundedBorder)
+                    .padding()
+                    .onSubmit {
+                        do {
+                            let prediction = try model.prediction(text: text)
+                            result = prediction.label
+                        }catch {
+                            print("failed to predict \(error.localizedDescription)")
+                        }
+                    }
+                if !result.isEmpty {
+                    Text(result)
+                        .foregroundStyle(color)
+                }
+                
+                Spacer()
+            }
+            .padding()
+            .navigationTitle("Financial News")
+        }
+        
+      
     }
 }
 
